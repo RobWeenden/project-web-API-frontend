2bdd13e1483f44060ae6b2bfa19cc0f976c65f57
RobWeenden <weendenandrade@gmail.com>
2021-10-22 14:56:39 -0300

    deploy modific enviro

--- src/app/app-constants.ts
+++ src/app/app-constants.ts
@@ -1,6 +1,7 @@
+import { environment } from './../environments/environment.prod';
 export class AppConstants {
 
-  public static get baseServer(): string { return 'http://localhost:8080/'}
+  public static get baseServer(): string { return environment.API}
   public static get baseLogin(): string {return this.baseServer + 'api-curso/login'}
   public static get baseUrl(): string {return this.baseServer +  'api-curso/usuario/v1'}
   public static get baseUrlPath(): string {return this.baseServer + 'api-curso'}
